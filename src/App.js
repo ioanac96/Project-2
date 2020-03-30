4c9b8de284015a53c6e6daacb675af3518464d80
Ciobanu Ioana <ciobanuioana@192-168-0-112.rdsnet.ro>
2020-03-30 14:34:46 +0300

    1.1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Login.js';
 import Register from './Register.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link
+} from "react-router-dom";
 
 
 class App extends React.Component {
@@ -44,6 +49,7 @@ class App extends React.Component {
     console.log(this.state);
     const action = this.state.currentAction;
     return (
+
       <div className="page">
         <div className="navigation-region">
             <div className={action === 'Login'? "navigation active":"navigation"} id='Login' onClick={this.currentAction('Login')}>Login</div>
